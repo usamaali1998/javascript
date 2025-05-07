c5e8f0d2aad81117af30adc3c0910401f5d599db
usamaali1998 <usama.ali2@yahoo.com>
2025-05-07 21:57:03 +0000

    update

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// 7 types : String Number Boolean Null undefined Symbol BigInt
+const score=100
+const scoreValue = 100.3
+const isLoogedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+const bigNumber = 1231231123123123123123123123n
+console.log(bigNumber)
+
+
+//non primitive (Reference)
+
+//array objects function
+
+const heros = ["shaktiman", "Goku", "superman"]
+let myObj = {
+    name : "Usama",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
