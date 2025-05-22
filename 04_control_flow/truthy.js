b327c3201ef73e06f5df4441420cb4a47c9434e6
usamaali1998 <usama.ali2@yahoo.com>
2025-05-22 16:20:20 +0000

    update

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,4 +1,17 @@
-const userEmail = "usama@gmail.co"
+/* flasy values 
+false, 0,-0,BigInt, 0n, undefined, NaN
+
+//truthy values
+rest all truthy vlaues, 
+"0", 'false', " " , [] ,{} , function(){}
+*/
+
+
+
+
+
+
+const userEmail = []
 if (userEmail) {
     console.log("got usr emai");
     
@@ -6,3 +19,30 @@ if (userEmail) {
     console.log("Dont have email");
     
 }
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+    
+}
+
+const emptyObject = {}
+if (Object.keys(emptyObject).length === 0) {
+    console.log("Object is empty");
+    
+}
+
+// Nullish Coalescing Operator (??) : null
+let val1;
+//val1 = 5??10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1= null ?? 100??10
+console.log(val1);
+
+// Ternary Operator
+
+//condition ? true : false
+
+const iceTeePrice = 100
+iceTeePrice >= 1001 ? console.log("less tan 90") : console.log("more than 90");
+
